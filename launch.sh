8746f0399147ad5db1d7c66ff9261e7130bfff5c
mharriso <mharriso@student.21-school.ru>
2021-01-29 09:33:19 +0300

    create launch script

---
+++ launch.sh
@@ -0,0 +1,19 @@
+	GOINFRE="/goinfre/$USER";
+
+	# set goinfre
+	echo $GOINFRE
+	mkdir $GOINFRE/Caches
+	mkdir $GOINFRE/Caches/com.docker.docker
+	rm -rf ~/Library/Caches/com.docker.docker
+	ln -s $GOINFRE/Caches/com.docker.docker ~/Library/Caches/com.docker.docker
+	mkdir $GOINFRE/Containers
+	mkdir $GOINFRE/.docker
+	rm -rf ~/.docker
+	ln -s $GOINFRE/.docker ~/.docker
+	mkdir $GOINFRE/Containers/com.docker.docker
+	rm -rf ~/Library/Containers/com.docker.docker
+	ln -s $GOINFRE/Containers/com.docker.docker ~/Library/Containers/com.docker.docker
+
+	#launch Docker Quickstart Terminal
+	cd /Applications
+	open Docker/Docker\ Quickstart\ Terminal.app
